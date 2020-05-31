f2169b257157c0a6b9b184f09f62e12780553918
Cristina <szakacscristina94@yahoo.com>
2020-05-31 13:32:01 +0300

    I finalised the RecommendationDto plus added recSERVICE, recImplementation and recRepository.

--- src/main/java/ro/ubbcluj/travelit/serviceapi/controller/mapper/RecommendationMapper.java
+++ src/main/java/ro/ubbcluj/travelit/serviceapi/controller/mapper/RecommendationMapper.java
@@ -1,7 +1,9 @@
 package ro.ubbcluj.travelit.serviceapi.controller.mapper;
 
 import ro.ubbcluj.travelit.serviceapi.controller.dto.RecommendationDto;
+import ro.ubbcluj.travelit.serviceapi.controller.dto.UserDto;
 import ro.ubbcluj.travelit.serviceapi.model.Recommendation;
+import ro.ubbcluj.travelit.serviceapi.model.User;
 
 public class RecommendationMapper {
     public static RecommendationDto mapToDto(final Recommendation recommendation) {
@@ -11,6 +13,14 @@ public class RecommendationMapper {
                 .setAddress(recommendation.getAddress())
                 .setName(recommendation.getName());
 
+    }
+
+    public static Recommendation mapToEntity(final RecommendationDto recommendationDto) {
+        return new Recommendation()
+                .setId(recommendationDto.getId())
+                .setName(recommendationDto.getName())
+                .setAddress(recommendationDto.getAddress())
+                .setDescription(recommendationDto.getDescription());
     }
 
 }
--- src/main/java/ro/ubbcluj/travelit/serviceapi/service/RecommendationService.java
+++ src/main/java/ro/ubbcluj/travelit/serviceapi/service/RecommendationService.java
@@ -1,4 +1,9 @@
 package ro.ubbcluj.travelit.serviceapi.service;
 
+import ro.ubbcluj.travelit.serviceapi.model.Recommendation;
+import ro.ubbcluj.travelit.serviceapi.model.User;
+
 public interface RecommendationService {
+
+    Recommendation save(Recommendation recommendation);
 }
